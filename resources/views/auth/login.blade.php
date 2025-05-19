947cff7387d7dc77333478633190dceb1cf4a773
itgithubrismahandd <rismahandayani801@gmail.com>
2025-05-19 14:18:48 +0700

    Simpan perubahan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -104,7 +104,7 @@
                     <div class="flex items-center justify-end">
                         <span class="text-xs text-black-500">
                             Forgot password?
-                            <a href="#" class="text-xs text-red-500 hover:text-blue-700">Click Here</a>
+                            <a href="/forgot-password" class="text-xs text-red-500 hover:text-blue-700">Click Here</a>
                         </span>
                     </div>
 
